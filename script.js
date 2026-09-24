2fd73f884d7889a002e704bbf61d8c20e0049d8e
dudacommits <devmeduarda@gmail.com>
2026-09-24 15:39:57 -0300

    updates

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
       loop: true,
       grabCursor: true,
       autoplay: {
-        delay: 8000, // 8 segundos
+        delay: 4000, // 8 segundos
         disableOnInteraction: false,
       },
       breakpoints: {
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
       loop: true,
       grabCursor: true,
       autoplay: {
-        delay: 8000, // 8 segundos
+        delay: 5000, // 8 segundos
         disableOnInteraction: false,
       },
       breakpoints: {
